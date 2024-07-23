9d72164819c1135d165a15d59ab3c708793bb2a7
Vinicius Lima <viniciuslimadacruz6@gmail.com>
2024-07-22 21:01:42 -0300

    Condição do botão whats e inserindo o icone da página

--- menu.js
+++ menu.js
@@ -1,7 +1,8 @@
 let menu = document.getElementById('menu-nav')
 let menuCor = document.getElementById('menu-muda-cor')
 let menuItem = document.querySelector('.menu-item')
-console.log(menuItem)
+let whats = document.querySelector('.icone-whats')
+
 
 document.addEventListener('scroll', rolar)
 
@@ -11,20 +12,28 @@ function rolar(){
 
     
     let posicaoAtual = window.scrollY;
+    console.log(posicaoAtual)
 
     if(posicaoAtual > ultimaPosicao){
-        document.getElementById('menu-nav').className = "teste";
+        document.getElementById('menu-nav').className = "esconde-menu";
     } else{
      document.getElementById('menu-nav').className = ""
     }
-    if(posicaoAtual > 800 && posicaoAtual < 1800 || posicaoAtual > 2800 && posicaoAtual < 3800 || posicaoAtual > 4300 && posicaoAtual < 5800){
-        menuCor.style.backgroundColor = "#606d80"
-        // menuItem.style.color = '#20A6FF';
-        // menuItem.style.fontWeight = '500';
-    } else{
-        menuCor.style.backgroundColor = ""
-        // menuItem.style.color = '';
-        // menuItem.style.fontWeight = '';
+    // if(posicaoAtual > 800 && posicaoAtual < 1800 || posicaoAtual > 2800 && posicaoAtual < 3800 || posicaoAtual > 4300 && posicaoAtual < 5800){
+    //     menuCor.style.backgroundColor = "#606d80"
+    //     // menuItem.style.color = '#20A6FF';
+    //     // menuItem.style.fontWeight = '500';
+    // } else{
+    //     menuCor.style.backgroundColor = ""
+    //     // menuItem.style.color = '';
+    //     // menuItem.style.fontWeight = '';
+    // }
+
+    if(posicaoAtual > 4750){
+        whats.style.display = 'none'
+    }else{
+        whats.style.display = ''
+
     }
 
    
