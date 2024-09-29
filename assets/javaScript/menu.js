70d3163f6ac1cf3c00327cc2e295e09bfe0a6a25
vinicius lima <viniciuslimadacruz6@gmail.com>
2024-09-29 17:45:53 -0300

    Inserindo transições no menu e envio do formulário

--- assets/javaScript/menu.js
+++ assets/javaScript/menu.js
@@ -10,9 +10,10 @@ let footer = document.getElementById('footer');//Captura o footer
 document.addEventListener("scroll", ()=>{//Evento de scroll
     //Verifica se o topo do footer - a altura da tela for menor ou igual a zero, o elemento whats irá sumir, se não o elemento o whats será mostrado
     if(footer.getBoundingClientRect().top - innerHeight <= 0){
-        document.querySelector("#icone-whats").style.display = "none"
+        document.querySelector("#icone-whats").style.right = "-50px"
+
     }else{
-        document.querySelector("#icone-whats").style.display = "block"
+        document.querySelector("#icone-whats").style.right= "100px"
     }
 })
 
@@ -24,10 +25,15 @@ document.addEventListener("scroll", ()=>{//Evento de scrolar a tela
     let posicaoAtual = scrollY
     //Se a posição atual for maior que a última posição que é zero, o menu será escondido, se não, o menu irá aparece.
     if(posicaoAtual > ultimaPosicao){
-        document.getElementById('menu-nav').className = "esconde-menu";
+        // document.getElementById('menu-nav').className = "esconde-menu";
+        document.querySelector(".fixed-top").style.top = "-150px"
+        document.querySelector(".fixed-top").style.transition = "top 1s"
      } else{
-           document.getElementById('menu-nav').className = "block"
+           document.querySelector(".fixed-top").style.top = "20px"
+           document.querySelector(".fixed-top").style.transition = "top 1s"
      }
      //ultima posição recebe a posição atual toda vez que a função for chamada, ou seja se a posição atual for 50 na última vez que a função for chamada esse será o valor atribuido a última posicao. Quando ocorre um novo if a posição atual receberá um novo valor, por exemplo 20, então 20 não será maior que 50, fazendo o menu aparecer, isso indica que o usuário está scrolando para cima. 
      ultimaPosicao = posicaoAtual;
 })
+
+
