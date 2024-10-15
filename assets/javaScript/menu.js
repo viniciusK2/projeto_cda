c2f854002f78d891873ca29be9921e95f82d67e6
vinicius lima <viniciuslimadacruz6@gmail.com>
2024-10-15 20:41:25 -0300

    Subindo página de obrigado, concertando alguns bugs

--- assets/javaScript/menu.js
+++ assets/javaScript/menu.js
@@ -29,7 +29,7 @@ document.addEventListener("scroll", ()=>{//Evento de scrolar a tela
         document.querySelector(".fixed-top").style.top = "-150px"
         document.querySelector(".fixed-top").style.transition = "top 1s"
      } else{
-           document.querySelector(".fixed-top").style.top = "20px"
+           document.querySelector(".fixed-top").style.top = "0px"
            document.querySelector(".fixed-top").style.transition = "top 1s"
      }
      //ultima posição recebe a posição atual toda vez que a função for chamada, ou seja se a posição atual for 50 na última vez que a função for chamada esse será o valor atribuido a última posicao. Quando ocorre um novo if a posição atual receberá um novo valor, por exemplo 20, então 20 não será maior que 50, fazendo o menu aparecer, isso indica que o usuário está scrolando para cima. 
